e699327fe5e32485fdab428b693cf2dbd0016cd1
goutamas <goutamas.keladi@gmail.com>
2025-07-06 19:14:00 +0530

    Implemented webpack production config

---
+++ framework/marketing/config/webpack.production.js
@@ -0,0 +1,24 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+    publicPath: "/marketing/latest/", // path in S3 bucket
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Marketing": "./src/bootstrap",
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
